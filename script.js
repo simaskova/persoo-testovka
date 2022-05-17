44ac895c4fb42c181a8305c8897121e628d564bc
Ema <emsimaskova@gmail.com>
2022-05-17 13:42:12 +0200

    cart notification functionality and responsivity fixes

--- script.js
+++ script.js
@@ -8,15 +8,24 @@ hideComponent = (className) => {
   !component.classList.contains("hide")? component.classList.add("hide") : "";
 }
 
+showComponent = (className) => {
+  var component = document.querySelector(className);
+  component.classList.contains("hide")? component.classList.remove("hide") : "";
+}
+
+let countItems = 0;
+
 // add to cart
 addToCart = () => {
-  let fullPath = event.target.parentElement.parentElement.parentElement.style.background.slice(5, 31);
+  let fullPath = event.target.classList.contains("add_to_cart__button")? event.target.parentElement.parentElement.style.background.slice(5, 31) : event.target.parentElement.parentElement.parentElement.style.background.slice(5, 31);
   let slicedPath = fullPath.slice(11, 19) + ".png"
 
-  let productName = event.target.parentElement.previousElementSibling.previousElementSibling.innerText;
+  let productName =  event.target.classList.contains("add_to_cart__button")? event.target.previousElementSibling.previousElementSibling.innerText : event.target.parentElement.previousElementSibling.previousElementSibling.innerText;
 
-  let productPrice = event.target.parentElement.previousElementSibling.innerText
+  let productPrice = event.target.classList.contains("add_to_cart__button")? event.target.previousElementSibling.innerText : event.target.parentElement.previousElementSibling.innerText
   
+  console.log(slicedPath + " " + productName + " " + productPrice)
+
   const cartItem = document.createElement("li")
   cartItem.classList.add("shopping_cart__item");
   cartItem.innerHTML = `
@@ -30,9 +39,22 @@ addToCart = () => {
      <p class="shopping_cart__item__price">${productPrice}</p>
   `;
 
+  // notify cart
+  const cartNotification = document.querySelector(".countItems");
+  countItems++;
+  cartNotification.innerText = countItems;
+
 
   // show in cart
   const listCartItems = document.querySelector(".shopping_cart__items");
   const checkoutButton = document.querySelector(".checkout__button")
   listCartItems.append(cartItem);
+}
+
+cartEmpty = () => { 
+  countItems === 0? (
+    hideComponent(".checkout__button"), showComponent(".empty_cart")
+  ) : (
+    showComponent(".checkout__button"), hideComponent(".empty_cart")
+  )
 }
