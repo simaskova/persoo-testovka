a879c586878405b8ea3ef2826069a4b833e9eb50
Ema <emsimaskova@gmail.com>
2022-05-23 10:32:51 +0200

    paths and js correction after html change

--- script.js
+++ script.js
@@ -18,19 +18,17 @@ let countItems = 0;
 // add to cart
 addToCart = () => {
   let fullPath = event.target.classList.contains("add_to_cart__button")? event.target.parentElement.parentElement.style.background.slice(5, 31) : event.target.parentElement.parentElement.parentElement.style.background.slice(5, 31);
-  let slicedPath = fullPath.slice(11, 19) + ".png"
+  let slicedPath = fullPath.slice(11, 19) + ".png";
 
-  let productName =  event.target.classList.contains("add_to_cart__button")? event.target.previousElementSibling.previousElementSibling.innerText : event.target.parentElement.previousElementSibling.previousElementSibling.innerText;
+  let productName =  event.target.classList.contains("add_to_cart__button")? event.target.previousElementSibling.firstElementChild.innerText : event.target.parentElement.previousElementSibling.firstElementChild.innerText;
 
-  let productPrice = event.target.classList.contains("add_to_cart__button")? event.target.previousElementSibling.innerText : event.target.parentElement.previousElementSibling.innerText
+  let productPrice = event.target.classList.contains("add_to_cart__button")? event.target.previousElementSibling.firstElementChild.nextElementSibling.innerText : event.target.parentElement.previousElementSibling.firstElementChild.nextElementSibling.innerText;
   
-  console.log(slicedPath + " " + productName + " " + productPrice)
-
   const cartItem = document.createElement("li")
   cartItem.classList.add("shopping_cart__item");
   cartItem.innerHTML = `
     <div class="shopping_cart__item__about">
-      <img src="/imgs/half/${slicedPath}" class="shopping_cart__item__picture" title="${productName}" ></img>
+      <img src="./imgs/half/${slicedPath}" class="shopping_cart__item__picture" title="${productName}" ></img>
       <div>
         <p class="shopping_cart__item__name">${productName}</p>
           <p class="shopping_cart__item__size">Size 11</p>
